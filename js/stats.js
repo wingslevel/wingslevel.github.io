14570567cf081189b3dbcc81ee4a4dda21586ced
WingsLevel <62509361+wingslevel@users.noreply.github.com>
2026-01-17 20:35:47 +0530

    Delete js directory

--- js/stats.js
+++
@@ -1,4 +0,0 @@
-const data = load();
-const mastered = data.filter(q => q.status === "mastered").length;
-const weak = data.filter(q => q.status === "weak").length;
-stats.innerText = `Total: ${data.length} | Mastered: ${mastered} | Weak: ${weak}`;
--- js/storage.js
+++
@@ -1,7 +0,0 @@
-const KEY = "wingslevel_questions";
-function load() {
-  return JSON.parse(localStorage.getItem(KEY) || "[]");
-}
-function save(data) {
-  localStorage.setItem(KEY, JSON.stringify(data));
-}
